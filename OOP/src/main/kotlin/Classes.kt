9bd697d49e43a8a492bc0e1274488eb7c315e12b
martinromario55 <martinromario55@gmail.com>
2025-01-28 22:21:57 +0300

    OOP

---
+++ OOP/src/main/kotlin/Classes.kt
@@ -0,0 +1,167 @@
+//class Car (name: String, var model: String, var color: String, var doors: Int) {
+//    var name = name.trim()
+//
+//    fun move() {
+//        println("The car $name is moving")
+//    }
+//
+//    fun stop() {
+//        println("The car $name has stopped")
+//    }
+//}
+
+//class User(name: String, var lastName: String, var age: Int) {
+//    var name: String
+//
+//    init {
+//        if (name.lowercase().startsWith("m")) {
+//            this.name = name
+//        } else {
+//            this.name = "User"
+//            println("The name doesn't start with the letter 'm' or 'M'.")
+//        }
+//    }
+//}
+
+//  Secondary Constructor
+
+//class User(var name: String, var lastName: String, var age: Int) {
+//    constructor(name: String): this(name, "Last Name", 0)
+//}
+
+//class User(var firstName: String = "John", var lastName: String = "Doe", var age: Int = 0)
+
+//  Companion Object
+class Calculator() {
+    companion object {
+        fun sum(a: Int, b: Int): Int {
+            return a + b
+        }
+    }
+}
+
+//  Singleton
+class Database private constructor() {
+    companion object {
+        private var instance: Database? = null
+
+        fun getInstance(): Database? {
+            if (instance == null) {
+                instance = Database()
+            }
+
+            return instance
+        }
+    }
+}
+
+//  Singleton Shortcode
+object DatabaseTwo {
+    init {
+        println("Database Created")
+    }
+}
+
+//  ENUM Classes
+enum class Direction(var direction: String, var distance: Int) {
+    NORTH("north", 10),
+    SOUTH("south", 20),
+    EAST("east", 15),
+    WEST("west", 40);
+
+    fun printData() {
+        println("Direction = $direction and Distance = $distance")
+    }
+}
+
+//  Inner Classes
+
+class ListView(val items: Array<String>) {
+    inner class ListViewItems() {
+        fun displayItems(position: Int) {
+            println(items[position])
+        }
+    }
+}
+
+//  Inheritance
+//open class Vehicle(val name: String, val color: String) { // Base Class
+//    open fun move() {
+//        println("$name is moving")
+//    }
+//
+//    fun stop() {
+//        println("$name has stopped")
+//    }
+//}
+//class Car(name: String, color: String, val engines: Int, val doors: Int): Vehicle(name, color) {
+//}
+//
+//class  Plane(name: String, color: String, val engines: Int, val doors: Int): Vehicle(name, color) {
+//    override fun move() {
+//        flying()
+////        super.move()
+//    }
+//
+//    fun flying() {
+//        println("The $name plane is flying")
+//    }
+//}
+
+
+//  Sealed Classes
+
+sealed class Result(val message: String) {
+    fun showMessage() {
+        println("Result: $message")
+    }
+    class Success(message: String): Result(message)
+    class Error(message: String): Result(message)
+}
+
+//  Abstract Classes
+abstract class Vehicle() {
+    abstract fun move()
+
+    abstract fun stop()
+}
+
+class Car(var name: String, var color: String, val engines: Int, val doors: Int): Vehicle() {
+    override fun move() {
+        TODO("Not yet implemented")
+    }
+
+    override fun stop() {
+        TODO("Not yet implemented")
+    }
+}
+
+//  Structural Equality
+//class User(var firstName: String, var lastName: String, var age: Int) {
+//
+//    override fun equals(other: Any?): Boolean {
+////        return super.equals(other)
+//        if (this === other) { // Referential Equality
+//            return true
+//        }
+//
+//        if (other is User) {
+//            return this.firstName == other.firstName && this.lastName == other.lastName && this.age == other.age
+//        }
+//
+//        return false
+//    }
+//
+//    override fun hashCode(): Int {
+////        return super.hashCode()
+//        return 0
+//    }
+//
+//    override fun toString(): String {
+////        return super.toString()
+//        return "User(firstName='$firstName', lastName='$lastName', age=$age)"
+//    }
+//}
+
+//  Structural Equality short code
+data class User(var firstName: String, var lastName: String, var age: Int)
---
+++ OOP/src/main/kotlin/Main.kt
@@ -0,0 +1,130 @@
+fun main() {
+//    val car1 = Car()
+
+//    car1.name = "Tesla"
+//    car1.model = "S Plaid"
+//    car1.color = "Red"
+//    car1.doors = 4
+
+//    println("Name = ${car1.name}")
+//    println("Model = ${car1.model}")
+//    println("Color = ${car1.color}")
+//    println("Doors = ${car1.doors}")
+//
+//    car1.move()
+//    car1.stop()
+
+
+//    val car2 = Car()
+
+//    car2.name = "Ford"
+//    car2.model = "Mustang"
+//    car2.color = "Black"
+//    car2.doors = 2
+
+//    println("Name = ${car2.name}")
+//    println("Model = ${car2.model}")
+//    println("Color = ${car2.color}")
+//    println("Doors = ${car2.doors}")
+
+//    Object Constructor
+
+//    val car1 = Car("Tesla", "S Plaid", "Red", 4)
+//    val car2 = Car("Ford", "Mustang", "Black", 2)
+//
+//        println("Name = ${car1.name}")
+//    println("Model = ${car1.model}")
+//    println("Color = ${car1.color}")
+//    println("Doors = ${car1.doors}")
+//
+//    println("\n")
+//        println("Name = ${car2.name}")
+//    println("Model = ${car2.model}")
+//    println("Color = ${car2.color}")
+//    println("Doors = ${car2.doors}")
+
+//    val user = User("Martin", "Romario", 30)
+//    val friend = User("James", "Dean", 45)
+
+
+//    val friend = User("James")
+
+//    val user = User()
+//
+//    val result = Calculator.sum(5,10)
+//
+//    println(result)
+
+//    val instance = Database.getInstance()
+//
+//    println(instance)
+
+//    println(DatabaseTwo)
+//    println(DatabaseTwo)
+
+//    println(Direction.WEST)
+//    for (direction in Direction.values()) {
+//        println(direction)
+//    }
+//    Direction.WEST.printData()
+
+//    val listView = ListView(arrayOf("Mathew", "Mark", "Luke", "John"))
+//    listView.ListViewItems().displayItems(2)
+
+//    val car = Car("BMW", "Red", 1, 4)
+//    val plane = Plane("Boeing", "White and Blue", 4,4)
+//
+//    car.move()
+//    car.stop()
+//
+//    plane.move()
+//    plane.stop()
+
+//    val success = Result.Success("Success!")
+//    val error = Result.Error("Failed")
+//
+//    getData(error)
+
+    val user1 = User("Martin", "Romario", 30)
+    val user2 = User("Martin", "Romario", 30)
+
+    println(user1 == user2)
+
+    println(user1)
+    println(user2)
+}
+
+//class Car {
+//    var name = ""
+//    var model = ""
+//    var color = ""
+//    var doors = 0
+//
+//    fun move() {
+//        println("The car $name is moving")
+//    }
+//
+//    fun stop() {
+//        println("The car $name has stopped")
+//    }
+//}
+
+//  Class Constructor
+
+//class Car (var name: String, var model: String, var color: String, var doors: Int) {
+//
+//    fun move() {
+//        println("The car $name is moving")
+//    }
+//
+//    fun stop() {
+//        println("The car $name has stopped")
+//    }
+//}
+
+fun getData(result: Result) {
+    when(result) {
+        is Result.Error -> result.showMessage()
+        is Result.Success -> result.showMessage()
+    }
+}
